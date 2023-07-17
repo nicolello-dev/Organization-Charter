8d23640bc1275ce12c56b40ad938f866550197b7
nicola-commits <nicolamigone179@gmail.com>
2023-07-17 16:40:40 +0200

    Added other hashmaps

--- src/components/viewData.tsx
+++ src/components/viewData.tsx
@@ -7,11 +7,13 @@ interface Team {
 }
 
 interface Area {
+    name: string
     lead: string | undefined
     teams: Team[]
 }
 
 interface FunctionType {
+    name: string
     lead: string | undefined
     areas: Area[]
 }
@@ -27,8 +29,8 @@ export default function ViewData(props: {data: Employee[] | undefined}){
         return <h1>Data not yet loaded. Please hang in while the request compiles. If this is the first request of the day, it might take up to 20 seconds</h1>
     }
     // Tribe -> Function -> Area -> Team
-    const tribeHash: Tribe = { lead: undefined, functions: []};
-    const functionHash: FunctionType = { lead: undefined, areas: []};
+    const tribeHash: {[key: string]: Tribe} = {};
+    const functionHash: {[key: string]: FunctionType} = {};
     const areaHash: {[key: string]: Area} = {};
     const teamHash: {[key: string]: Team} = {};
     for (const employee of data) {
@@ -50,33 +52,61 @@ export default function ViewData(props: {data: Employee[] | undefined}){
                 area.teams.push(team);
             } else {
                 areaHash[team.employees[0]?.area] = {
+                    name: team.employees[0].area,
                     lead: team.employees[0].area_lead,
                     teams: [team]
                 }
             }
         }
     }
-    console.log(areaHash);
+    for (const area of Object.values(areaHash)) {
+        if(area.teams[0]?.employees[0]) {
+            const fun: FunctionType | undefined = functionHash[area.teams[0].employees[0]?.function]
+            if(fun) {
+                fun.areas.push(area);
+            } else {
+                functionHash[area.teams[0].employees[0]?.function] = {
+                    name: area.teams[0].employees[0].function,
+                    lead: area.teams[0].employees[0].function_lead,
+                    areas: [area]
+                }
+            }
+        }
+    }
+    for (const fun of Object.values(functionHash)) {
+        if(fun.areas[0]?.teams[0]?.employees[0]) {
+            const tribe: Tribe | undefined = tribeHash[fun.areas[0].teams[0].employees[0]?.tribe]
+            if(tribe) {
+                tribe.functions.push(fun);
+            } else {
+                tribeHash[fun.areas[0].teams[0].employees[0]?.tribe] = {
+                    lead: fun.areas[0].teams[0].employees[0].tribe_lead,
+                    functions: [fun]
+                }
+            }
+        }
+    }
+    console.log(tribeHash)
     return (
         <div className="flex flex-wrap justify-center">
             {
                 Object.keys(areaHash).map(area_name => {
-                    const area_lead = teamHash[area_name]?.lead;
-                    return <div key={"area"+area_name} className="w-6/12 max-w-lg text-center border-2 m-3 p-3 rounded-2xl text-black">
+                    const area_lead = areaHash[area_name]?.lead;
+                    return <div key={"area"+area_name} className="w-6/12 max-w-lg text-center border-2 m-3 p-3 rounded-2xl text-black border-area/25">
                         <div className="m-3">
                             <h3>{area_name}</h3>
                             <h5>{area_lead}</h5>
                         </div>
                         {
-                        areaHash[area_name]?.teams.map((team, i) => <div key={"team" + i.toString()} className="p-3 m-2 rounded-xl border-2">
+                        areaHash[area_name]?.teams.map((team, i) => <div key={"team" + i.toString()} className="p-3 m-2 rounded-xl border-2 border-black/25">
                             <div className="m-3">
                                 <h3>{team.name}</h3>
                                 <h5>{team.lead}</h5>
                             </div>
                             <div className="flex flex-row flex-wrap justify-center">
-                                {team.employees.map(e => <pre key={"name"+e.team_member} className="m-2">
+                                {team.employees.map((e, i) => <p key={"name"+i.toString()} className="m-2">
                                         {e.team_member}
-                                    </pre>
+                                    </p>
                                 )}
                             </div>
                         </div>)
