3cb332de2affe7279f46325b527117e19c1379e0
Ilaria <82510639+ilariiiiia@users.noreply.github.com>
2023-08-21 00:05:39 +0200

    Added dummy filter buttons

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,7 +1,25 @@
 export default function Filters() {
+
+    const filters = [
+        'team', 'area', 'function', 'tribe'
+    ];
+
     return (
         <>
-            
+        <h1 className="text-xl m-6">
+            Filters:
+        </h1>
+            {
+                filters.map(filter => 
+                        <>
+                            <p className="m-2">{filter}:</p>
+                            <input className="border-gray-300 border-2 mb-4" type="text"/>
+                        </>
+                    )
+            }
+        <button className="bg-black text-white p-3 m-4 rounded-xl">
+            Filter
+        </button>
         </>
     )
 }
