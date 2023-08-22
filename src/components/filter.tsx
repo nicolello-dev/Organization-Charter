7f554684ce80c480eb20bd280079d3d4b80f0b53
Ilaria <82510639+ilariiiiia@users.noreply.github.com>
2023-08-22 19:26:23 +0200

    Updated filters UI

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -9,14 +9,18 @@ export default function Filters() {
         <h1 className="text-xl m-6">
             Filters:
         </h1>
+        <div className="flex flex-row gap-4 flex-wrap justify-center">
             {
                 filters.map((filter, i) => 
                         <div key={i} className="text-center">
                             <p className="m-2">{filter}:</p>
                             <input className="border-gray-300 border-2 mb-4" type="text"/>
+                            <p className="m-2">{filter} lead:</p>
+                            <input className="border-gray-300 border-2 mb-4" type="text"/>
                         </div>
                     )
             }
+        </div>
         <button className="bg-black text-white p-3 m-4 rounded-xl">
             Filter
         </button>
