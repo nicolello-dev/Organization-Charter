c5502d3c9f48b8d1cf492c98a39569bff21ff916
Ilaria <82510639+ilariiiiia@users.noreply.github.com>
2023-08-22 19:44:06 +0200

    Updated prisma/client.ts to recommended

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,3 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export default globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = globalForPrisma.prisma || new PrismaClient()
+}
