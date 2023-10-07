c6ae1acedad46b48f11cac137b7e82fdf906bdc5
nicolello <nicolamigone179@gmail.com>
2023-10-07 17:16:25 +0200

    Teams are shown horizontally

--- components/domain.tsx
+++ components/domain.tsx
@@ -11,10 +11,12 @@ export default function Domain({ employees, domainName, color }: { employees: Em
     return <div className={`${visualClasses} border-[${color}] bg-[${color}]`}>
         <h3 className="text-xl">{domainName || "No domain"}</h3>
         <p className="p-1 bg-blue-400 w-max">{employees[0]?.team.domain_lead || "No domain lead"}</p>
+        <div className="flex flex-row flex-wrap justify-center">
         {
             Object.entries(teams).map(([teamName, employees], i) => {
                 return <Team key={i} teamName={teamName} employees={employees} />
             })
         }
+        </div>
     </div>
 }
