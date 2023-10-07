a43faf56cf698457e7933c39bc912295c422f332
nicolello <nicolamigone179@gmail.com>
2023-10-07 17:12:34 +0200

    Updated border color

--- components/team.tsx
+++ components/team.tsx
@@ -3,7 +3,7 @@ import { EmployeeWithTeam } from "@/types/common/employees";
 import { visualClasses } from "@/constants/constants";
 
 export default function Team({ teamName, employees }: { teamName: string, employees: EmployeeWithTeam[] }) {
-    return <div className={`${visualClasses} border-blue-400 border-dashed`}>
+    return <div className={`${visualClasses} border-white border-dashed`}>
     <h4 className="text-lg">{teamName}</h4>
     <p className="p-1 bg-blue-400 w-max">{employees[0]?.team.team_lead || "No team lead"}</p>
         {
