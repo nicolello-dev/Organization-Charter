8bb988d8fbfaf2af504e1da96439191c704f747e
nicolello-dev <nicolamigone179@gmail.com>
2024-01-23 18:58:59 +0100

    Delete unused file

--- lib/components/viewData.ts
+++
@@ -1,9 +0,0 @@
-import type { Team } from "@/types/common/teams";
-
-import fetch from "node-fetch"
-
-export async function getTeamInfo(team: string): Promise<Team | null> {
-    /* Returns related data of the team as specified in the teams.json file */
-    const teamData = await (await fetch("https://organization-charter.nicolello.repl.co/teams.json")).json() as Team[];
-    return teamData.find(t => t.Team == team) || null;
-}
