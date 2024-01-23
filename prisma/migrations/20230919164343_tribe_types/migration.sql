f09a3d4f37dae2515006979b2d083dd2f8189b28
nicolello-dev <nicolamigone179@gmail.com>
2024-01-23 11:23:00 +0100

    Removed migrations and test data

--- prisma/migrations/20230919164343_tribe_types/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Type" AS ENUM ('Global', 'ProductOffering', 'RegionalBusiness');
-
--- AlterTable
-ALTER TABLE "Team" ADD COLUMN     "type" "Type" NOT NULL DEFAULT 'Global';
