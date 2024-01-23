6439f65361f1744cc1863776544e7962a8db9fe4
nicolello-dev <nicolamigone179@gmail.com>
2024-01-23 11:20:36 +0100

    Removed new instance of prisma client, using global scoped one

--- pages/api/getFilteredEmployees.ts
+++ pages/api/getFilteredEmployees.ts
@@ -1,59 +1,67 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient, type Employee, Team } from '@prisma/client';
+import { prisma } from "@/prisma/prisma";
 
 import type { Filter } from "@/types/common/filters";
-
-const prisma = new PrismaClient();
+import type { EmployeeWithTeam } from "@/types/common/employees";
 
 type Request = NextApiRequest & {
-    query: {
-        name: string,
-        functionalLead: string,
-        teamName: string,
-        teamLead: string,
-        domain: string,
-        domainLead: string,
-        tribeArea: string,
-        tribeAreaLead: string,
-        tribe: string,
-        tribeLead: string
-    }
-}
+  query: {
+    name: string;
+    functionalLead: string;
+    teamName: string;
+    teamLead: string;
+    domain: string;
+    domainLead: string;
+    tribeArea: string;
+    tribeAreaLead: string;
+    tribe: string;
+    tribeLead: string;
+  };
+};
 
-interface EmployeeWithTeam extends Employee {
-    team: Team;
-}
+export default async function Handler(
+  req: Request,
+  res: NextApiResponse<EmployeeWithTeam[]>,
+) {
+  const {
+    name,
+    functionalLead,
+    teamName,
+    teamLead,
+    domain,
+    domainLead,
+    tribeArea,
+    tribeAreaLead,
+    tribe,
+    tribeLead,
+  } = req.query as Filter;
 
-export default async function Handler(req: Request, res: NextApiResponse<EmployeeWithTeam[]>) {
-
-    const { name, functionalLead, teamName, teamLead, domain, domainLead, tribeArea, tribeAreaLead, tribe, tribeLead } = req.query as Filter;
-
-    try {
-        // Thank you ChatGPT :)
-        const employees = await prisma.employee.findMany({
-            where: {
-                name: {
-                    contains: name
-                },
-                functional_lead: functionalLead || undefined,
-                team: {
-                    name: teamName || undefined,
-                    team_lead: teamLead || undefined,
-                    domain: domain || undefined,
-                    domain_lead: domainLead || undefined,
-                    tribe_area: tribeArea || undefined,
-                    tribe_area_lead: tribeAreaLead || undefined,
-                    tribe: tribe || undefined,
-                    tribe_lead: tribeLead || undefined
-                },
-            },
-            include: {
-                team: true
-            }
-        });
-        res.status(200).json(employees);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json([]);
-    }
+  try {
+    // Thank you ChatGPT :)
+    const employees = await prisma.employee.findMany({
+      where: {
+        name: {
+          contains: name,
+        },
+        functional_lead: functionalLead || undefined,
+        team: {
+          name: teamName || undefined,
+          team_lead: teamLead || undefined,
+          domain: domain || undefined,
+          domain_lead: domainLead || undefined,
+          tribe_area: tribeArea || undefined,
+          tribe_area_lead: tribeAreaLead || undefined,
+          tribe: tribe || undefined,
+          tribe_lead: tribeLead || undefined,
+        },
+      },
+      include: {
+        team: true,
+      },
+    });
+    res.status(200).json(employees);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json([]);
+  }
 }
--- types/common/teams.ts
+++
@@ -1,8 +0,0 @@
-export type Team = {
-    Team: string
-    "Team Lead": string
-    Domain: string
-    "Domain Lead": string
-    "Tribe Area": string
-    "Tribe Area Lead": string
-}
